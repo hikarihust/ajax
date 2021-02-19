fdd948830e95ed0b6722dbbf4e1b6ffee07aac9b
vudinhquang <vudinhquangk53@gmail.com>
2021-02-19 15:51:27 +0900

    Practice -Show Film

--- loadmore/assets/js/my-js.js
+++ loadmore/assets/js/my-js.js
@@ -1,6 +1,8 @@
 let ITEM_PER_PAGE = 4;
 let page = 0;
 let flag = false;
+let filmHtmlTemplate = $('#templateHtml');
+let showFilmsElement = $('#show-films');
 
 
 $(document).ready(function () {
@@ -17,7 +19,20 @@ function loadData() {
         },
         dataType: "json",
         success: function (data) {
-            console.log(data);
+            appendData(data.items, filmHtmlTemplate, showFilmsElement);
         }
     });
+}
+
+function appendData(items, filmHtmlTemplate, showFilmsElement) {
+    if (items.length > 0) {
+        $.each(items, (index, value) => {
+            let htmlMore = filmHtmlTemplate.html();
+            htmlMore = htmlMore.replace(/{image}/g, value.image);
+            htmlMore = htmlMore.replace(/{title}/g, value.title);
+            htmlMore = htmlMore.replace(/{description}/g, value.description);
+            showFilmsElement.append(htmlMore);
+        });
+    }
+    $("body, html").animate({scrollTop: $(document).height()}, 500);
 }
