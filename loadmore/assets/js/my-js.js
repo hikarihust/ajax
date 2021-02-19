86e044b9983bfd37cb5a13387bfdd2a9c7efe8bd
vudinhquang <vudinhquangk53@gmail.com>
2021-02-19 15:38:36 +0900

    Prepare - Ajax

--- loadmore/assets/js/my-js.js
+++ loadmore/assets/js/my-js.js
@@ -1,3 +1,23 @@
 let ITEM_PER_PAGE = 4;
 let page = 0;
 let flag = false;
+
+
+$(document).ready(function () {
+    loadData();
+});
+
+function loadData() {
+    $.ajax({
+        type: "GET",
+        url: "load_more.php",
+        data: {
+            limit : ITEM_PER_PAGE + 1,
+            offset : page * ITEM_PER_PAGE
+        },
+        dataType: "json",
+        success: function (data) {
+            
+        }
+    });
+}
