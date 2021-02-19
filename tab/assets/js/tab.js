d7975173ae13c2fc61e27301c4e0e24de5aa2ac2
vudinhquang <vudinhquangk53@gmail.com>
2021-02-20 03:08:49 +0900

    Prepare - Show Film

--- tab/assets/js/tab.js
+++ tab/assets/js/tab.js
@@ -1,10 +1,25 @@
 let filmHtmlTemplate = $('#templateHtml');
 let showFilmsElement = $('.content-movie');
+let categoryId = 1;
 
 
 $(document).ready(function(){
-
+    loadData(categoryId);
 });
+
+function loadData(categoryId) {
+	$.ajax({
+		url		: 'tab.php',
+		data	: {
+		type: 'load-data', 
+		id: categoryId
+		},
+		type	: 'GET',
+		dataType: 'json'
+	}).done(function(data){
+		appendData(data, filmHtmlTemplate, showFilmsElement);
+	});
+}
 
 function appendData(items, filmHtmlTemplate, showFilmsElement) {
 	if (items.length > 0) {
--- tab/tab.php
+++ tab/tab.php
@@ -1,3 +1,10 @@
 <?php
 require_once 'libs/database.php';
 $database = new Database();
+$type = $database->removeXss($_GET['type']);
+
+if($type == 'load-data') {
+	$categoryId = $database->removeXss($_GET['id']);
+	$result = $database->getArrayDataByCategoryId($categoryId);
+	echo json_encode($result);
+}
