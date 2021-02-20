64bab4a62d80d5f1baf1a21d30626fe3ae367882
vudinhquang <vudinhquangk53@gmail.com>
2021-02-20 16:04:01 +0900

    Practice - Show Tooltip

--- tooltip/assets/js/tooltip.js
+++ tooltip/assets/js/tooltip.js
@@ -31,8 +31,28 @@ function loadFull() {
 	}).done(function() {
         $('.tooltip-custom').tooltipster({
             plugins: ['follower'],
+            contentAsHTML: true,
+            interactive: true,
             functionBefore: function(instance, helper) {
-                instance.content('My new content');
+                let id = instance._$origin[0].id;
+                $.ajax({
+                    url		: 'tooltip.php',
+                    data	: {
+                        type: 'load-one',
+                        id : id
+                    },
+                    type	: 'GET',
+                    dataType: 'json',
+                    async: false
+                }).done(function(data){
+                    let content =    '<div>Name:   '+ data.title +'</div>';
+                        content +=    '<div>Type:   '+ data.category_name +'</div>';
+                        content +=    '<div>Actor:  '+ data.actor_name +'</div>';
+                        content +=    '<div>Release Date: '+ data.description +'</div>';
+                        content +=    '<div>Detail Type: '+ data.category_description +'</div>';
+                        $('#demo-position-content').html(content);
+                        instance.content($('#demo-position-content').html());
+                })
             }
         });
     })
--- tooltip/tooltip.php
+++ tooltip/tooltip.php
@@ -7,3 +7,9 @@ if($type == "load-full") {
     $result = $database->getArray('item');
     echo json_encode($result);
 }
+
+if($type == "load-one") {
+    $id		= $database->removeXss($_GET['id']);
+    $result = $database->getArrayDataToolTip($id);
+    echo json_encode($result[0]);
+}
