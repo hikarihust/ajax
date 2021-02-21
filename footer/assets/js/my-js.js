bf7280c134aaf1ddbaa8dd24b1cddf6461e44136
vudinhquang <vudinhquangk53@gmail.com>
2021-02-21 17:13:28 +0900

    Practice - Click

---
+++ footer/assets/js/my-js.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    $('#subscribe-form').click(function(e) {
+        let value = $('.subscribe-form').val();
+        if(isEmail(value)) {
+            displayMsg(false, true);
+        } else {
+            displayMsg(true, false);
+        }
+    })
+});
+
+function isEmail($email) {
+    if($email == '') {
+        return false;
+    }
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test($email);
+}
+
+function displayMsg ($error = false, $success = false) {
+    $error == true ? $('.error-subscript').css('display', 'block') : $('.error-subscript').css('display', 'none');
+    $success == true ? $('.success-subscript').css('display', 'block') : $('.success-subscript').css('display', 'none');
+}
