8190c725ae1d2c7193728889ed3438dc63a2f088
vudinhquang <vudinhquangk53@gmail.com>
2021-02-21 16:53:39 +0900

    Prepare - Setup

---
+++ footer/index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Load More</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/font.css">
+	<link rel="stylesheet" href="assets/css/my-css.css">
+</head>
+<body>
+	<div class="container">
+		<h3 class="center">07.Thực hành phần 2</h3>
+	</div>
+	<div class="footer-custom">
+		<footer id="footer" class="dark">
+			<div class="container">
+				<!-- Footer Widgets
+				============================================= -->
+				<div class="footer-widgets-wrap clearfix">
+					<div class="col_two_third">
+						<div class="col_one_third">
+							<div class="widget clearfix">
+								<img alt="" class="footer-logo">
+								<p>COFFEE chúng tôi luôn khao khát mang lại 
+									những giá trị đích thực của cà phê, cũng như những 
+									giá trị tốt đẹp của cuộc sống đến với mọi người</p>
+							</div>
+						</div>
+						<div class="col_one_third">
+							<div class="widget widget_links clearfix">
+								<h4>Sản phẩm</h4>
+								<ul>
+									<li><a href="#">Cà phê</a></li>
+									<li><a href="#">Công nghệ pha chế</a></li>
+									<li><a href="#">Nhượng quyền</a></li>
+									<li><a href="#">Kinh doanh</a></li>
+								</ul>
+							</div>
+						</div>
+						<div class="col_one_third col_last">
+							<div class="widget clearfix">
+								<h4>Thông tin</h4>
+								<div>
+									<ul class="widget widget_links">
+										<li><a href="#">Về chúng tôi</a></li>
+									</ul>
+									<abbr title="Phone Number"><strong>Phone:</strong></abbr> (91) 8547 632521<br>
+									<abbr title="Email Address"><strong>Email:</strong></abbr> vanduy@canvas.com
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col_one_third col_last">
+						<div class="widget subscribe-widget clearfix">
+							<h5><strong>Đăng kí</strong> để nhận những thông báo về chúng tôi sớm nhất.</h5>
+							<div class="widget-subscribe-form-result"></div>
+							<form id="widget-subscribe-form" action="javascript:void(0);" method="post" class="nobottommargin">
+								<div class="input-group divcenter">
+									<div class="input-group-prepend">
+										<div class="input-group-text"><i class="icon-email2"></i></div>
+									</div>
+									<input type="email"  name="subscribe-form" class="form-control required email subscribe-form" placeholder="Enter your Email">
+									<div class="input-group-append">
+										<button class="btn btn-success" id="subscribe-form" type="submit">Subscribe</button>
+									</div>
+									
+								</div>
+								<h5 class="error-subscript">Đây không phải là một email</h5>
+								<h5 class="success-subscript">Bạn đã đăng kí thành công</h5>
+							</form>
+						</div>
+						<div class="widget clearfix" style="margin-bottom: -20px;">
+							<div class="row">
+								<div class="col-lg-6 clearfix bottommargin-sm">
+									<a href="#" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;">
+										<i class="icon-facebook"></i>
+										<i class="icon-facebook"></i>
+									</a>
+									<a href="#"><small style="display: block; margin-top: 3px;"><strong>Like us</strong><br>on Facebook</small></a>
+								</div>
+								<div class="col-lg-6 clearfix">
+									<a href="#" class="social-icon si-dark si-colored si-rss nobottommargin" style="margin-right: 10px;">
+										<i class="icon-rss"></i>
+										<i class="icon-rss"></i>
+									</a>
+									<a href="#"><small style="display: block; margin-top: 3px;"><strong>Subscribe</strong><br>to RSS Feeds</small></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+	</div>
+	<script src="assets/js/jquery.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/my-js.js"></script>
+</body>
+</html>
